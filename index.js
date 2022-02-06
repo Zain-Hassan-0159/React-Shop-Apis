b54338436e5fbfdb367a4324e2543d01a4f53c4f
Zain Hassan <zainhassan927@gmail.com>
2022-02-06 22:47:24 +0500

    Product APIs done

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 const authRoute = require("./routes/auth")
 
 dotenv.config();
 
 // DataBase Connection Mongdb
 mongoose.connect(process.env.Mongo_url)
-.then(()=>console.log("hurray!"))
+.then(()=>console.log("hurray! Database is connected"))
 .catch((err)=>{console.log(err)});
 
 // Routes
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // Port
 app.listen(process.env.PORT || 5000, ()=>{
